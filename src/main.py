59bd2c5f44aae96637bf899cad6cf701eeff17ab
Peter Kovac <pojzinko8@gmail.com>
2024-09-09 18:18:07 +0200

    Training time (not working yet)

--- src/main.py
+++ src/main.py
@@ -1,22 +1,46 @@
 import os.path as ospath
-
 from preprocessing import DataLoader
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, Flatten
+import numpy as np
 
 data_path = "../data"
 fake_news_path = ospath.join(data_path, "Fake.csv")
 real_news_path = ospath.join(data_path, "True.csv")
 
-train_dataloader = DataLoader(fake_news_path, nrows=1)
-train_dataloader.init_data(['title', 'text', 'subject'], 1)
+real_dataloader = DataLoader(fake_news_path, nrows=100)
+real_dataloader.init_data(['title', 'text', 'subject'], 1)
+real_dataloader.preprocess_data()
 
-features, labels = train_dataloader.get()
-print(len(features[0][1]))
+fake_dataloader = DataLoader(fake_news_path, nrows=100)
+fake_dataloader.init_data(['title', 'text', 'subject'], 0)
+fake_dataloader.preprocess_data()
 
-train_dataloader.preprocess_data()
-print()
-print()
+real_features, real_labels, real_max_len = real_dataloader.get()
+fake_features, fake_labels, fake_max_len = fake_dataloader.get()
 
-features, labels = train_dataloader.get()
-print(len(features[0][1]))
+# merge the two numpy arrays
+all_features = np.concatenate((real_features, fake_features), axis=0)
+all_labels = np.concatenate((real_labels, fake_labels), axis=0)
 
-# print(features[:5])
+# shuffle the data
+indices = np.arange(all_features.shape[0])
+np.random.shuffle(indices)
+all_features = all_features[indices]
+all_labels = all_labels[indices]
+
+model = Sequential()
+model.add(Embedding(real_dataloader.get_vocab_size(), 32, input_length=real_max_len))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(all_features, all_labels, epochs=100, batch_size=32)
+
+real_prediction = model.predict(real_features)
+fake_prediction = model.predict(fake_features)
+
+print(f"Real news prediction: {real_prediction}", len(real_prediction))
+print(f"Fake news prediction: {fake_prediction}", len(fake_prediction))
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,6 @@
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
 from tensorflow.keras.preprocessing.text import Tokenizer
+import re
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -15,38 +14,82 @@ class DataLoader:
    
     def init_data(self, columns: list[str], label: int):
         self.features_raw = self.data[columns].to_numpy()
+        for i in range(len(self.features_raw)):
+            for j in range(len(self.features_raw[i])):
+                self.features_raw[i][j] = re.split(r'\s+', self.features_raw[i][j])
+
         self.num_features = len(columns)
         self.labels = [label] * len(self.data)
     
-    def _remove_stop_words(self, string: str) -> str:
+    def get_vocab_size(self):
+        return len(self.all_tokens)
+    
+    def _remove_stop_words(self, words: list[str]) -> str:
         result = []
-        for word in string.split():
+        for word in words:
             if word not in self.stopwords:
                 result.append(word)
         
         return " ".join(result)
     
-    def _to_lower(self, string: str) -> str:
-        return string.lower()
+    def _to_lower(self, words: list[str]) -> list[str]:
+        return [word.lower() for word in words]
     
     def _tokenize(self):
-        self.features = []
+        self.all_tokens = set()
+        tokenized_1 = []
         for i in range(len(self.features_raw)):
             cur_row = []
             for column in self.features_raw[i]:
-                tokenizer_column = word_tokenize(column)
-                cur_row.append(tokenizer_column)
+                tokenized_column = word_tokenize(column)
+                cur_row.append(tokenized_column)
+                self.all_tokens.update(tokenized_column)
+
+            tokenized_1.append(cur_row)
+        
+        self.tokenizer = Tokenizer(num_words=len(self.all_tokens))    
+        self.tokenizer.fit_on_texts(self.all_tokens)
+        self.features = []
+        for i in range(len(tokenized_1)):
+            cur_row = []
+            for column in tokenized_1[i]:
+                cur_row.append(self.tokenizer.texts_to_sequences(column))
 
             self.features.append(cur_row)
+        
     
     def preprocess_data(self):
         for i in range(len(self.features_raw)):
             for j in range(self.num_features):
-                self.features_raw[i][j] = self._remove_stop_words(self.features_raw[i][j])
                 self.features_raw[i][j] = self._to_lower(self.features_raw[i][j])
+                self.features_raw[i][j] = self._remove_stop_words(self.features_raw[i][j])
         
         self._tokenize()
     
-    
     def get(self):
-        return (self.features_raw, self.labels)
+        def pad_sequence(sequence, max_length):
+            return np.pad(sequence, (0, max_length - len(sequence)), 'constant')
+
+        max_len = 0
+        flattened_features = []
+        for row in self.features:
+            cur_row = []
+            for column in row:
+                cur_column = []
+                for token in column:
+                    if isinstance(token, list):
+                        cur_column.extend(token)
+                    else:
+                        cur_column.append(token)
+
+                max_len = max(max_len, len(cur_column))
+                cur_row.append(cur_column)
+            
+            flattened_features.append(cur_row)
+        
+        max_len *= 2
+        for i in range(len(flattened_features)):
+            for j in range(len(flattened_features[i])):
+                flattened_features[i][j] = pad_sequence(flattened_features[i][j], max_len)
+
+        return np.array(flattened_features), np.array(self.labels), max_len
